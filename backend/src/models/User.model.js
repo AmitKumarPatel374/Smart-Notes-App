7d9cd4c8ec52e4ef11e56dba46e452a5f67c8efc
Amit Kumar Patel <amitpatel9302352967@gmail.com>
2026-03-07 10:00:00 +0530

    feat(footer): implemented backend usermodel setup

---
+++ backend/src/models/User.model.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next){
+    if (this.isModified("password")) {
+        if (!this.password.startsWith("$2b$")) {
+            this.password = await bcrypt.hash(this.password,11);
+        }
+    }
+    next();
+})
+
+userSchema.methods.coparePass = async function (password){
+    let pass = await bcrypt.compare(password,this.password);
+    return pass;
+}
+
+userSchema.methods.generateToken = function(){
+  let token = jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  })
+  return token
+}
+
+const userModel = mongoose.model("user",userSchema);
+
+module.exports = userModel;
+
