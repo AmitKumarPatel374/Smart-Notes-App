aa09e940739d6b88680c6afdd6451468af25fed0
Amit Kumar Patel <amitpatel9302352967@gmail.com>
2026-03-09 11:00:00 +0530

    feat(db): setup database debug

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,20 @@
 const express = require("express")
-const cors = require("cors");
-const connectDB = require("./src/config/db");
-require("dotenv").config();
+const cors = require("cors")
+const connectDB = require("./src/config/db")
+require("dotenv").config()
 
-connectDB();
-const app = express();
-app.use(express.json());
+connectDB()
+const app = express()
+app.use(express.json())
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
-);
+)
 
-const port = process.env.PORT;
-app.listen(port, ()=>{
-    console.log("app is running in port 3000");
+const port = process.env.PORT
+app.listen(port, () => {
+  console.log("app is running in port 3000")
 })
--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,15 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
-const connectDB = async ()=>{
-    try {
-        let res = await mongoose.connect(process.env.MONGO_URI);
-        
-        if (res) {
-            console.log("mongodb connected successfully!")
-        }
-    } catch (error) {
-        console.log("error in  connecting db -->",error)
+const connectDB = async () => {
+  try {
+    let res = await mongoose.connect(process.env.MONGO_URI)
+
+    if (res) {
+      console.log("mongodb connected successfully!")
     }
+  } catch (error) {
+    console.log("error in  connecting db -->", error)
+  }
 }
 
-module.exports = connectDB;
+module.exports = connectDB
