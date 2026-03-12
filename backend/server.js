097b46da6790609a681a3ab3056af9a798fc730f
Amit Kumar Patel <amitpatel9302352967@gmail.com>
2026-03-12 10:00:00 +0530

    configure app.js and server.js as my folder structure

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,10 @@
 const express = require("express")
 const cors = require("cors")
 const connectDB = require("./src/config/db")
+const app  = require("./src/app")
 require("dotenv").config()
 
 connectDB()
-const app = express()
-app.use(express.json())
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-)
-
 const port = process.env.PORT
 app.listen(port, () => {
   console.log("app is running in port 3000")
---
+++ backend/src/app.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const authRoutes = require("../src/routes/auth.routes")
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+)
+app.use("/api/auth",)
+
+
+module.exports = app;
