88eef443f0268a4fa040c475d702d7693f0532dd
Amit Kumar Patel <amitpatel9302352967@gmail.com>
2026-04-26 22:28:00 +0530

    implemented authentication on frontend

--- frontend/src/context/DataContext.jsx
+++ frontend/src/context/DataContext.jsx
@@ -1,9 +1,25 @@
 import React, { createContext, useState } from "react"
+import apiInstance from "../api/apiInstance"
 
 const userContext = createContext(null)
 const DataContext = (props) => {
-  const { token, setToken } = useState(null)
-  return <userContext.Provider value={{ token }}>{props.children}</userContext.Provider>
+  const  [token, setToken ] = useState(false)
+  const  [authLoading, setAuthLoading ] = useState(true)
+
+  const checkAuth=async()=>{
+     try {
+       setAuthLoading(true);
+       const response = await apiInstance.get("/user/profile");
+       if (response && response.data) {
+        setToken(true);
+       }
+     } catch (error) {
+      setToken(false);
+     }finally{
+      setAuthLoading(false);
+     }
+  }
+  return <userContext.Provider value={{ token,authLoading }}>{props.children}</userContext.Provider>
 }
 
 export default DataContext
